3328a1933ac85f4c386b2b11e3f7f21a751cfa1d
jise8893 <60957575+jise8893@users.noreply.github.com>
2020-10-08 20:13:01 +0900

    Update dllmain.cpp

--- NtAddAtomEx+JS/dllmain.cpp
+++ NtAddAtomEx+JS/dllmain.cpp
@@ -1,18 +1,16 @@
-﻿#include "pch.h"
+// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다.
+#include "pch.h"
+#include <Windows.h>
 #include <detours.h>
-#include <stdio.h>
-#include <processthreadsapi.h>
 #include <winternl.h>
-#include <iostream>
+#include <stdio.h>
 #include "atombomb.h"
 #define DLLBASIC_API extern "C" __declspec(dllexport)
-#pragma comment(lib, "detours.lib")
-#define STATUS_SUCCESS               ((NTSTATUS)0x00000000L)
 
-
-static pfNtAddAtomEx NtAddAtomEx =NULL;
-static pNtQueueApcThread NtQueueApcThread=NULL;
-
+static LONG dwSlept = 0;
+static pNtQueueApcThread NtQueueApcThread = NULL;
+static DWORD(WINAPI* TrueSleepEx)(DWORD dwMilliseconds, BOOL bAlertable) = SleepEx;
+static LPVOID(WINAPI* TrueVirtualAllocEx)(HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize,DWORD flAllocationType,DWORD flProtect) = VirtualAllocEx;
 DLLBASIC_API NTSTATUS NTAPI MyNtQueueApcThread(
     HANDLE ThreadHandle,
     PVOID ApcRoutine,
@@ -28,69 +26,70 @@ DLLBASIC_API NTSTATUS NTAPI MyNtQueueApcThread(
         ApcReserved OPTIONAL);
 }
 
-DLLBASIC_API NTSTATUS NTAPI MyNtAtomEx(
-    PWSTR String,
-    ULONG StringLength,
-    PUSHORT Atom,
-    ULONG Unknown
-)
-{
-    printf("MyNtAtomEx is used\n");
+DLLBASIC_API LPVOID WINAPI DetectVirtualAllocEx(HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect) {
     
-    wprintf(L"%s\n", String);
+    printf("VirtualAllocEx is used\n");
+    return TrueVirtualAllocEx(hProcess, lpAddress, dwSize, flAllocationType, flProtect);
 
-    NtAddAtomEx(String,
-        StringLength,
-        Atom,
-        Unknown);
-    return STATUS_SUCCESS;
+}
+DLLBASIC_API DWORD WINAPI DetectSleepEx(DWORD dwMilliseconds, BOOL bAlertable) {
+    DWORD dwBeg = GetTickCount();
+    DWORD ret = TrueSleepEx(dwMilliseconds, bAlertable);
+    DWORD dwEnd = GetTickCount();
+    InterlockedExchangeAdd(&dwSlept, dwEnd - dwBeg);
+    
+    return ret;
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
-{
-    
-    (void)hinst;
-    (void)reserved;
-    HMODULE hMod = NULL;
-    switch (dwReason)
-    {
 
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    LONG error;
+
+    if (DetourIsHelperProcess())
+        return TRUE;
+
+    switch (ul_reason_for_call)
+    {
     case DLL_PROCESS_ATTACH:
-        if (hMod != NULL)
-        {
-            hMod = GetModuleHandleA("ntdll.dll");
-            NtAddAtomEx = (pfNtAddAtomEx)GetProcAddress(hMod, "NtAddAtomEx");
-            NtQueueApcThread = (pNtQueueApcThread)GetProcAddress(hMod, "NtQueueApcThread");
-        }
+        hModule = GetModuleHandleA("ntdll.dll");
+        NtQueueApcThread = (pNtQueueApcThread)GetProcAddress(hModule, "NtQueueApcThread");
+
         DetourRestoreAfterWith();
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        if (NtAddAtomEx != NULL)
-            DetourAttach(&(PVOID&)NtAddAtomEx, MyNtAtomEx);
+        DetourAttach(&(PVOID&)TrueSleepEx, DetectSleepEx);
+        DetourAttach(&(PVOID&)TrueVirtualAllocEx, DetectVirtualAllocEx);
         if (NtQueueApcThread != NULL)
             DetourAttach(&(PVOID&)NtQueueApcThread, MyNtQueueApcThread);
-
-        printf("DLL_PROCESS_ATTACH\n");
-
-        DetourTransactionCommit();
+        error = DetourTransactionCommit();
+        if (error == NO_ERROR) {
+            printf(DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
+                " Detoured start().\n");
+        }
+        else {
+            printf("simple" DETOURS_STRINGIFY(DETOURS_BITS) ".dll:"
+                " Error detouring SleepEx(): %ld\n", error);
+        }
         break;
     case DLL_THREAD_ATTACH:
-        printf("DLL_THREAD_ATTACH\n");
         break;
     case DLL_THREAD_DETACH:
-        printf("DLL_THREAD_DETACH\n");
         break;
     case DLL_PROCESS_DETACH:
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        if (NtAddAtomEx != NULL)
-            DetourDetach(&(PVOID&)NtAddAtomEx, MyNtAtomEx);
+        DetourDetach(&(PVOID&)TrueSleepEx, DetectSleepEx);
+        DetourDetach(&(PVOID&)TrueVirtualAllocEx, DetectVirtualAllocEx);
         if (NtQueueApcThread != NULL)
             DetourDetach(&(PVOID&)NtQueueApcThread, MyNtQueueApcThread);
-        DetourTransactionCommit();
-        printf("DLL_PROCESS_DETACH\n");
+        error = DetourTransactionCommit();
+        printf("Removed SleepEx() (result=%ld),slept %ld ticks\n", error, dwSlept);
         break;
     }
-
     return TRUE;
 }
+
