6119eb2e1e2d96bae550ab4ff8122aa2f536d4bc
Rua-A <62979202+Rua-A@users.noreply.github.com>
2020-10-14 23:29:53 +0900

    Update monitor

--- Rua-A/N4_Hook/dllmain.cpp
+++ Rua-A/N4_Hook/dllmain.cpp
@@ -8,19 +8,58 @@
 #include "pch.h"
 #include "detours.h"
 
+#define MSG_SIZE 256
+
+static NTMAPVIEWOFSECTION pNtMapViewOfSection;
+static NTCREATETHREADEX pNtCreateThreadEx;
+static NTALLOCATEVIRTUALMEMORY pNtAllocateVirtualMemory;
+static NTWRITEVIRTUALMEMORY pNtWriteVirtualMemory;
+static NTPROTECTVIRTUALMEMORY pNtProtectVirtualMemory;
+static NTQUERYSYSTEMINFORMATION pNtQuerySystemInformation;  // for getting system info
+
+//#####################################monitor
+
+static HANDLE hProcess = NULL;
+
+static LPVOID monMMF = NULL;
+static LPVOID dllMMF = NULL;
+
+static LPTHREAD_START_ROUTINE  CallNtAllocateVirtualMemory = NULL;
+static LPTHREAD_START_ROUTINE  CallNtProtectVirtualMemory = NULL;
+static LPTHREAD_START_ROUTINE  CallNtWriteVirtualMemory = NULL;
+static LPTHREAD_START_ROUTINE  CallNtCreateThreadEx = NULL;
+static LPTHREAD_START_ROUTINE  CallNtMapViewOfSection = NULL;
+static LPTHREAD_START_ROUTINE  CallCreateFileMappingA = NULL;
+static LPTHREAD_START_ROUTINE  CallNtGetThreadContext = NULL;
+static LPTHREAD_START_ROUTINE  CallNtSetThreadContext = NULL;
+static LPTHREAD_START_ROUTINE  CallNtQueueApcThread = NULL;
+static LPTHREAD_START_ROUTINE  CallSetWindowLongPtrA = NULL;
+static LPTHREAD_START_ROUTINE  CallSleepEx = NULL;
+
+static NTSTATUS(*PNtMapViewOfSection)(
+	HANDLE SectionHandle,
+	HANDLE ProcessHandle,
+	PVOID* BaseAddress,
+	ULONG_PTR ZeroBits,
+	SIZE_T CommitSize,
+	PLARGE_INTEGER SectionOffset,
+	PSIZE_T ViewSize,
+	SECTION_INHERIT InheritDisposition,
+	ULONG AllocationType,
+	ULONG Win32Protect);
+//#####################################
+
 static DBGPRINT pDbgPrint; // for debug printing 
 static NTSUSPENDTHREAD pNtSuspendThread;
 static NTGETCONTEXTTHREAD pNtGetThreadContext;
 static NTSETCONTEXTTHREAD pNtSetThreadContext;
 static RESUMETHREAD pNtResumeThread;
 
-CONTEXT old_ctx, new_ctx;
+//CONTEXT old_ctx, new_ctx; last use
 RUNTIME_MEM_ENTRY* result;
 HMODULE hMod = NULL;
 DWORD64 old_result,new_result;
-
-//TargetThreadHandle
-
+CONTEXT old_ctx, new_ctx;
 
 //NtSuspendThread
 DLLBASIC_API NTSTATUS NTAPI NtSuspendThread(
@@ -39,11 +78,12 @@ DLLBASIC_API NTSTATUS NTAPI NtGetThreadContext(
 	PCONTEXT pContext
 ) {
 	pDbgPrint("1N4_HOOK: PID=%d, NtGetThreadContext is hooked!\n", GetCurrentProcessId());
-	old_ctx.ContextFlags = CONTEXT_INTEGER;
-	pContext = &old_ctx;
-	pDbgPrint("1N4_HOOK: DETECTED GetThreadContext\n");
-	pDbgPrint("1CONTEXT.Rip : %016I64X\n", old_ctx.Rip);
-	printf("1OLD_CONTEXT.Rip : % 016I64X\n", old_ctx.Rip);
+	//old_ctx.ContextFlags = CONTEXT_ALL;
+	//pContext = &old_ctx;
+	//pDbgPrint("1N4_HOOK: DETECTED GetThreadContext\n");
+	//pDbgPrint("1CONTEXT.Rip : %016I64X\n", old_ctx.Rip);
+	//printf("1OLD_CONTEXT.Rip : % 016I64X\n", old_ctx.Rip);
+	//printf("1OLD_CONTEXT.Rip : %X\n", old_ctx);
 	return (*pNtGetThreadContext)(
 		ThreadHandle,
 		pContext
@@ -56,11 +96,11 @@ DLLBASIC_API NTSTATUS NTAPI NtSetThreadContext(
 	PCONTEXT lpContext
 ) {
 	pDbgPrint("1N4_HOOK: PID=%d, NtSetThreadContext is hooked!\n", GetCurrentProcessId());
-	new_ctx.ContextFlags = CONTEXT_INTEGER;
-	lpContext = &new_ctx;
-	pDbgPrint("1N4_HOOK: DETECTED SetThreadContext\n");
-	pDbgPrint("1CONTEXT.Rip : %016I64X\n", new_ctx.Rip);
-	printf("1NEW_CONTEXT.Rip : % 016I64X\n", new_ctx.Rip);
+	//new_ctx.ContextFlags = CONTEXT_INTEGER;
+	//lpContext = &new_ctx;
+	//pDbgPrint("1N4_HOOK: DETECTED SetThreadContext\n");
+	//pDbgPrint("1CONTEXT.Rip : %016I64X\n", new_ctx.Rip);
+	//printf("1NEW_CONTEXT.Rip : % 016I64X\n", new_ctx.Rip);
 	return (*pNtSetThreadContext)(
 		ThreadHandle,
 		lpContext
@@ -78,12 +118,47 @@ DLLBASIC_API NTSTATUS NTAPI NtResumeThread(
 
 BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
 {
-	(void)hinst;
-	(void)reserved;
+	hinst;
+	dwReason;
+	reserved;
+
+	HANDLE hMemoryMap = NULL;
+	LPBYTE pMemoryMap = NULL;
+
+	HANDLE fm;
+	char* map_addr;
+
+	LPVOID lpMap = 0;
+	SIZE_T viewsize = 0;
+
+	int sz;
+
 	HMODULE hMod = NULL;
 	switch (dwReason)
 	{
 	case DLL_PROCESS_ATTACH:
+	
+		//#############################monitor
+		hMemoryMap = OpenFileMappingA(FILE_MAP_READ, FALSE, (LPCSTR)"shared");
+
+		pMemoryMap = (BYTE*)MapViewOfFile(
+			hMemoryMap, FILE_MAP_READ,
+			0, 0, 0
+		);
+		if (!pMemoryMap)
+		{
+
+			CloseHandle(hMemoryMap);
+			printf("MapViewOfFile Failed.\n");
+			return FALSE;
+
+		}
+
+		sz = strlen((char*)pMemoryMap) + 1;
+
+		printf("%s\n", (char*)pMemoryMap);
+		printf("%d\n", *(DWORD*)((char*)pMemoryMap + sz));
+
 		// get ntdll module
 		hMod = GetModuleHandleA("ntdll.dll");
 		if (hMod == NULL) {
@@ -91,6 +166,36 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
 			return 1;
 		}
 		// get functions in ntdll
+		pNtMapViewOfSection = (NTMAPVIEWOFSECTION)GetProcAddress(hMod, "NtMapViewOfSection");
+		if (pNtMapViewOfSection == NULL) {
+			printf("CreationHook: Error - cannot get NtMapViewOfSection's address.\n");
+			return 1;
+		}
+		pNtCreateThreadEx = (NTCREATETHREADEX)GetProcAddress(hMod, "NtCreateThreadEx");
+		if (pNtCreateThreadEx == NULL) {
+			printf("CreationHook: Error - cannot get NtCreateThreadEx's address.\n");
+			return 1;
+		}
+		pNtAllocateVirtualMemory = (NTALLOCATEVIRTUALMEMORY)GetProcAddress(hMod, "NtAllocateVirtualMemory");
+		if (pNtAllocateVirtualMemory == NULL) {
+			printf("CreationHook: Error - cannot get NtAllocateVirtualMemory's address.\n");
+			return 1;
+		}
+		pNtWriteVirtualMemory = (NTWRITEVIRTUALMEMORY)GetProcAddress(hMod, "NtWriteVirtualMemory");
+		if (pNtWriteVirtualMemory == NULL) {
+			printf("CreationHook: Error - cannot get NtWriteVirtualMemory's address.\n");
+			return 1;
+		}
+		pNtQuerySystemInformation = (NTQUERYSYSTEMINFORMATION)GetProcAddress(hMod, "NtQuerySystemInformation");
+		if (pNtQuerySystemInformation == NULL) {
+			printf("CreationHook: Error - cannot get NtQuerySystemInformation's address.\n");
+			return 1;
+		}
+		pNtProtectVirtualMemory = (NTPROTECTVIRTUALMEMORY)GetProcAddress(hMod, "NtProtectVirtualMemory");
+		if (pNtProtectVirtualMemory == NULL) {
+			printf("CreationHook: Error - cannot get NtProtectVirtualMemory's address.\n");
+			return 1;
+		}
 		pNtSuspendThread = (NTSUSPENDTHREAD)GetProcAddress(hMod, "NtSuspendThread");
 		if (pNtSuspendThread == NULL) {
 			printf("N4Hook: Error - cannot find NtSuspendThread's address.\n");
@@ -116,14 +221,37 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
 			printf("N4Hook: Error - cannot get DbgPrint's address.\n");
 			return 1;
 		}
+		fm = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, MSG_SIZE, NULL);
+		map_addr = (char*)MapViewOfFile(fm, FILE_MAP_ALL_ACCESS, 0, 0, 0);
 
+		hProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_CREATE_THREAD, FALSE, *(DWORD*)((char*)pMemoryMap + sz));
+
+		(*pNtMapViewOfSection)(fm, hProcess, &lpMap, 0, MSG_SIZE, nullptr, (PULONG)(&viewsize), ViewUnmap, 0, PAGE_READWRITE); // "The default behavior for executable pages allocated is to be marked valid call targets for CFG." (https://docs.microsoft.com/en-us/windows/desktop/api/memoryapi/nf-memoryapi-mapviewoffile)
+
+		monMMF = (LPVOID)lpMap;
+		dllMMF = (LPVOID)map_addr;
+
+		CallNtAllocateVirtualMemory = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD)));
+		CallNtProtectVirtualMemory = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + sizeof(DWORD64)));
+		CallNtWriteVirtualMemory = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 2 * sizeof(DWORD64)));
+		CallNtCreateThreadEx = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 3 * sizeof(DWORD64)));
+		CallNtMapViewOfSection = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 4 * sizeof(DWORD64)));
+		CallCreateFileMappingA = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 5 * sizeof(DWORD64)));
+		CallNtGetThreadContext = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 6 * sizeof(DWORD64)));
+		CallNtSetThreadContext = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 7 * sizeof(DWORD64)));
+		CallNtQueueApcThread = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 8 * sizeof(DWORD64)));
+		CallSetWindowLongPtrA = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 9 * sizeof(DWORD64)));
+		CallSleepEx = (LPTHREAD_START_ROUTINE)(*(DWORD64*)(pMemoryMap + sz + sizeof(DWORD) + 10 * sizeof(DWORD64)));
+
+		printf("%llu\n", *(DWORD64*)(pMemoryMap + sz + sizeof(DWORD)));
+		//#############################
 		DetourRestoreAfterWith();
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
 
 		DetourAttach(&(PVOID&)pNtSuspendThread, NtSuspendThread);
 		DetourAttach(&(PVOID&)pNtGetThreadContext, NtGetThreadContext);
-		DetourAttach(&(PVOID&)pNtSetThreadContext, NtSetThreadContext);	
+		DetourAttach(&(PVOID&)pNtSetThreadContext, NtSetThreadContext);
 		DetourAttach(&(PVOID&)pNtResumeThread, NtResumeThread);
 		DetourTransactionCommit();
 		printf("DLL_PROCESS_ATTACH\n");
--- Rua-A/N4_Hook/framework.h
+++ Rua-A/N4_Hook/framework.h
@@ -41,3 +41,256 @@ typedef struct {
 	SIZE_T tot_write;
 	SIZE_T tot_alloc;
 } RUNTIME_MEM_ENTRY;
+#include <cstdint>
+
+/// @brief log level
+#define log_level_debug         3
+#define log_level_info          2
+#define log_level_warn          1
+#define log_level_critical      0
+#define log_level_error         log_level_critical
+
+/// @brief	ntdll::DbgPrintEx 
+///			(ref) dpfilter.h
+#define DPFLTR_ERROR_LEVEL 0
+#define DPFLTR_WARNING_LEVEL 1
+#define DPFLTR_TRACE_LEVEL 2
+#define DPFLTR_INFO_LEVEL 3
+#define DPFLTR_MASK 0x80000000
+
+#define DPFLTR_IHVDRIVER_ID 77
+
+typedef LONG_PTR(NTAPI* TrueNtUserSetWindowLongPtr)(
+	HWND hWnd,
+	DWORD Index,
+	LONG_PTR NewValue,
+	BOOL Ansi);
+
+typedef NTSTATUS(NTAPI* TrueNtAllocateVirtualMemory)(
+	HANDLE ProcessHandle,
+	PVOID* BaseAddress,
+	ULONG_PTR ZeroBits,
+	PSIZE_T RegionSize,
+	ULONG AllocationType,
+	ULONG Protect
+	);
+
+typedef NTSTATUS(NTAPI* TrueNtWriteVirtualMemory)(
+	HANDLE ProcessHandle,
+	PVOID BaseAddress,
+	PVOID Buffer,
+	ULONG NumberOfBytesToWrite,
+	PULONG NumberOfBytesWritten
+	);
+
+typedef enum _SECTION_INHERIT {
+	ViewShare = 1,
+	ViewUnmap = 2
+} SECTION_INHERIT, * PSECTION_INHERIT;
+
+// NtMapViewOfSection Function Pointer Type
+typedef NTSTATUS(NTAPI* NTMAPVIEWOFSECTION)(
+	HANDLE SectionHandle,
+	HANDLE ProcessHandle,
+	PVOID* BaseAddress,
+	ULONG ZeroBits,
+	ULONG CommitSize,
+	PLARGE_INTEGER SectionOffset,
+	PULONG ViewSize,
+	SECTION_INHERIT InheritDisposition,
+	ULONG AllocationType,
+	ULONG Protect
+	);
+
+// Struct types for NtCreateThreadEx
+typedef struct _UNICODE_STRING {
+	WORD Length;
+	WORD MaximumLength;
+	WORD* Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES {
+	ULONG Length;
+	PVOID RootDirectory;
+	PUNICODE_STRING ObjectName;
+	ULONG Attributes;
+	PVOID SecurityDescriptor;
+	PVOID SecurityQualityOfService;
+} OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+
+typedef struct _THREAD_INFO {
+	ULONG   Flags;
+	ULONG   BufferSize;
+	PVOID   lpBuffer;
+	ULONG   Unknown;
+} THREAD_INFO, * PTHREAD_INFO;
+
+typedef struct _CREATE_THREAD_INFO {
+	ULONG       Length;
+	THREAD_INFO Client;
+	THREAD_INFO TEB;
+} CREATE_THREAD_INFO;
+
+// NtCreateThreadEx Function Pointer Type
+typedef NTSTATUS(NTAPI* NTCREATETHREADEX)(
+	PHANDLE ThreadHandle,
+	ACCESS_MASK DesiredAccess,
+	POBJECT_ATTRIBUTES ObjectAttributes,
+	HANDLE ProcessHandle,
+	LPTHREAD_START_ROUTINE StartAddress,
+	LPVOID Parameter,
+	BOOL CreateSuspended,
+	DWORD StackZeroBits,
+	DWORD SizeOfStackCommit,
+	DWORD SizeOfStackReserve,
+	CREATE_THREAD_INFO* ThreadInfo
+	);
+
+// NtAllocateVirtualMemory Function Pointer Type
+typedef NTSTATUS(NTAPI* NTALLOCATEVIRTUALMEMORY)(
+	HANDLE ProcessHandle,
+	PVOID* BaseAddress,
+	ULONG_PTR ZeroBits,
+	PSIZE_T RegionSize,
+	ULONG AllocationType,
+	ULONG Protect
+	);
+
+// NtWriteVirtualMemory Function Pointer Type
+typedef NTSTATUS(NTAPI* NTWRITEVIRTUALMEMORY)(
+	HANDLE ProcessHandle,
+	PVOID BaseAddress,
+	PVOID Buffer,
+	ULONG NumberOfBytesToWrite,
+	PULONG NumberOfBytesWritten
+	);
+
+// NtProtectVirtualMemory Function Pointer Type
+typedef NTSTATUS(NTAPI* NTPROTECTVIRTUALMEMORY)(
+	HANDLE ProcessHandle,
+	PVOID* BaseAddress,
+	PULONG NumberOfBytesToProtect,
+	ULONG NewAccessProtection,
+	PULONG OldAccessProtection
+	);
+
+// DbgPrint Function Pointer Type
+typedef NTSTATUS(NTAPI* DBGPRINT)(
+	LPCSTR Format,
+	...
+	);
+
+// Enumeration & struct types for NtQuerySystemInformation
+typedef enum _SYSTEM_INFORMATION_CLASS {
+	SystemBasicInformation,
+	SystemProcessorInformation,
+	SystemPerformanceInformation,
+	SystemTimeOfDayInformation,
+	SystemPathInformation,
+	SystemProcessInformation,
+	SystemCallCountInformation,
+	SystemDeviceInformation,
+	SystemProcessorPerformanceInformation,
+	SystemFlagsInformation,
+	SystemCallTimeInformation,
+	SystemModuleInformation,
+	SystemLocksInformation,
+	SystemStackTraceInformation,
+	SystemPagedPoolInformation,
+	SystemNonPagedPoolInformation,
+	SystemHandleInformation,
+	SystemObjectInformation,
+	SystemPageFileInformation,
+	SystemVdmInstemulInformation,
+	SystemVdmBopInformation,
+	SystemFileCacheInformation,
+	SystemPoolTagInformation,
+	SystemInterruptInformation,
+	SystemDpcBehaviorInformation,
+	SystemFullMemoryInformation,
+	SystemLoadGdiDriverInformation,
+	SystemUnloadGdiDriverInformation,
+	SystemTimeAdjustmentInformation,
+	SystemSummaryMemoryInformation,
+	SystemNextEventIdInformation,
+	SystemEventIdsInformation,
+	SystemCrashDumpInformation,
+	SystemExceptionInformation,
+	SystemCrashDumpStateInformation,
+	SystemKernelDebuggerInformation,
+	SystemContextSwitchInformation,
+	SystemRegistryQuotaInformation,
+	SystemExtendServiceTableInformation,
+	SystemPrioritySeperation,
+	SystemPlugPlayBusInformation,
+	SystemDockInformation,
+	_SystemPowerInformation,
+	SystemProcessorSpeedInformation,
+	SystemCurrentTimeZoneInformation,
+	SystemLookasideInformation
+} SYSTEM_INFORMATION_CLASS, * PSYSTEM_INFORMATION_CLASS;
+
+typedef LONG KPRIORITY;
+typedef LONG KWAIT_REASON;
+#define STATUS_INFO_LENGTH_MISMATCH (0xC0000004L)
+
+typedef struct _VM_COUNTERS {
+	SIZE_T PeakVirtualSize;
+	SIZE_T VirtualSize;
+	ULONG PageFaultCount;
+	// Padding here in 64-bit
+	SIZE_T PeakWorkingSetSize;
+	SIZE_T WorkingSetSize;
+	SIZE_T QuotaPeakPagedPoolUsage;
+	SIZE_T QuotaPagedPoolUsage;
+	SIZE_T QuotaPeakNonPagedPoolUsage;
+	SIZE_T QuotaNonPagedPoolUsage;
+	SIZE_T PagefileUsage;
+	SIZE_T PeakPagefileUsage;
+} VM_COUNTERS;
+
+typedef struct _CLIENT_ID
+{
+	HANDLE UniqueProcess;
+	HANDLE UniqueThread;
+} CLIENT_ID, * PCLIENT_ID;
+
+typedef struct _SYSTEM_THREAD {
+	LARGE_INTEGER           KernelTime;
+	LARGE_INTEGER           UserTime;
+	LARGE_INTEGER           CreateTime;
+	ULONG                   WaitTime;
+	PVOID                   StartAddress;
+	CLIENT_ID               ClientId;
+	KPRIORITY               Priority;
+	LONG                    BasePriority;
+	ULONG                   ContextSwitchCount;
+	ULONG                   State;
+	KWAIT_REASON            WaitReason;
+} SYSTEM_THREAD, * PSYSTEM_THREAD;
+
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+	ULONG            NextEntryDelta;
+	ULONG            ThreadCount;
+	ULONG            Reserved1[6];
+	LARGE_INTEGER   CreateTime;
+	LARGE_INTEGER   UserTime;
+	LARGE_INTEGER   KernelTime;
+	UNICODE_STRING  ProcessName;
+	KPRIORITY        BasePriority;
+	ULONG            ProcessId;
+	ULONG            InheritedFromProcessId;
+	ULONG            HandleCount;
+	ULONG            Reserved2[2];
+	VM_COUNTERS        VmCounters;
+	IO_COUNTERS        IoCounters;
+	SYSTEM_THREAD  Threads[1];
+} SYSTEM_PROCESS_INFORMATION, * PSYSTEM_PROCESS_INFORMATION;
+
+// NtQuerySystemInformation Function Pointer Type
+typedef NTSTATUS(NTAPI* NTQUERYSYSTEMINFORMATION)(
+	SYSTEM_INFORMATION_CLASS SystemInformationClass,
+	PVOID SystemInformation,
+	ULONG SystemInformationLength,
+	PULONG ReturnLength
+	);
